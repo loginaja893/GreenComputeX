b9ecacb5d63fd4ce19c6261b79ab5dbca72fc9a4
loginaja893 <loginaja893@gmail.com>
2026-04-22 12:03:01 +0100

    Update main.sol

--- main.sol
+++ main.sol
@@ -73,3 +73,78 @@ library GCXBytes {
     function slice(bytes memory b, uint256 offset, uint256 len) internal pure returns (bytes memory out) {
         if (b.length < offset + len) revert GCX_BadBytesSlice();
         out = new bytes(len);
+        for (uint256 i = 0; i < len; ++i) out[i] = b[offset + i];
+    }
+}
+
+library GCXAddress {
+    function isContract(address a) internal view returns (bool) {
+        return a.code.length != 0;
+    }
+}
+
+library GCXSafeTransfer {
+    error GCX_ERC20TransferFailed();
+    error GCX_ERC20TransferFromFailed();
+    error GCX_ERC20ApproveFailed();
+    error GCX_ETHER_TRANSFER_FAILED();
+
+    function safeTransfer(IERC20Minimal token, address to, uint256 amount) internal {
+        (bool ok, bytes memory data) = address(token).call(
+            abi.encodeWithSelector(IERC20Minimal.transfer.selector, to, amount)
+        );
+        if (!ok || (data.length != 0 && !abi.decode(data, (bool)))) revert GCX_ERC20TransferFailed();
+    }
+
+    function safeTransferFrom(IERC20Minimal token, address from, address to, uint256 amount) internal {
+        (bool ok, bytes memory data) = address(token).call(
+            abi.encodeWithSelector(IERC20Minimal.transferFrom.selector, from, to, amount)
+        );
+        if (!ok || (data.length != 0 && !abi.decode(data, (bool)))) revert GCX_ERC20TransferFromFailed();
+    }
+
+    function safeApprove(IERC20Minimal token, address spender, uint256 amount) internal {
+        (bool ok, bytes memory data) = address(token).call(
+            abi.encodeWithSelector(IERC20Minimal.approve.selector, spender, amount)
+        );
+        if (!ok || (data.length != 0 && !abi.decode(data, (bool)))) revert GCX_ERC20ApproveFailed();
+    }
+
+    function safeTransferETH(address to, uint256 amount) internal {
+        (bool ok, ) = to.call{value: amount}("");
+        if (!ok) revert GCX_ETHER_TRANSFER_FAILED();
+    }
+}
+
+library GCXECDSA {
+    error GCX_BadSig();
+    error GCX_BadSigS();
+    error GCX_BadSigV();
+
+    function recover(bytes32 digest, bytes memory signature) internal pure returns (address) {
+        if (signature.length != 65) revert GCX_BadSig();
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+        assembly {
+            r := mload(add(signature, 0x20))
+            s := mload(add(signature, 0x40))
+            v := byte(0, mload(add(signature, 0x60)))
+        }
+        if (uint256(s) > 0x7fffffffffffffffffffffffffffffff5d576e7357a4501ddfe92f46681b20a0) revert GCX_BadSigS();
+        if (v != 27 && v != 28) revert GCX_BadSigV();
+        address signer = ecrecover(digest, v, r, s);
+        if (signer == address(0)) revert GCX_BadSig();
+        return signer;
+    }
+
+    function toEthSignedMessageHash(bytes32 hash) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
+    }
+}
+
+abstract contract GCXReentrancy {
+    uint256 private _gcxGate;
+    modifier nonReentrant() {
+        if (_gcxGate == 2) revert GCX_ReentrancyTripwire();
+        _gcxGate = 2;
